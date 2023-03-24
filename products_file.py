f57f7630a2b23519490cfd6a90cb4e3ca1c10826
wanchin0302 <128444456+wanchin0302@users.noreply.github.com>
2023-03-24 15:34:01 +0800

    Update products_file.py

--- products_file.py
+++ products_file.py
@@ -12,6 +12,7 @@ for p in products: #for loop 搞清楚每個東西是什麼
 	print(p)
 	print(p[0], "的價格是", p[1])
 
-with open("products.csv", "w") as f:  #可產出CSV檔
+with open("products.csv", "w", encoding="utf-8") as f:  #可產出CSV檔
+	f.write("商品,價格\n")
 	for p in products:
 		f.write(p[0] + "," + str(p[1]) + "\n")
