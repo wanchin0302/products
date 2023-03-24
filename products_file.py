39b6afe110083d8a1329be2ec21b1143801f4c42
wanchin0302 <128444456+wanchin0302@users.noreply.github.com>
2023-03-24 14:48:22 +0800

    Add files via upload

---
+++ products_file.py
@@ -0,0 +1,16 @@
+products = []
+while True:
+    name = input("請輸入商品名稱: ")
+    if name == "q":
+        break	
+    price = input("請輸入價格: ")
+    products.append([name,price])
+print(products)
+
+for p in products: #for loop 搞清楚每個東西是什麼
+	print(p)
+	print(p[0], "的價格是", p[1])
+
+with open("products.csv", "w") as f:
+	for p in products:
+		f.write(p[0] + "," + p[1] + "\n")
